5a8dac710a20b24916fd941bcd8748fb8f86a827
Timothy Landwehr <tjlandwehr@gmail.com>
2017-11-07 15:38:30 -0600

    Added a random movie feature and a 'tomorrow's movie' feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -17,13 +18,23 @@ def index():
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    tom_movie = get_random_movie()
+    while tom_movie == movie:
+        tom_movie = get_random_movie()
+
+    content += "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + tom_movie + "</li>"
+    content += "</ul>"
 
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movies = ["The Big Lebowski", "Inception", "Harry Potter and the Goblet of Fire", "Land Before Time", "Groundhog Day", "Saving Private Ryan"]
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    random_movie = movies[random.randint(0, len(movies) - 1)]
+    return random_movie
 
 
 app.run()
