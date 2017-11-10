799274571a8a2f5021ab05b5618d087017d7174f
Timothy Landwehr <tjlandwehr@gmail.com>
2017-11-09 18:51:50 -0600

    Changed get_random_movie() function slightly and updated content return in index

--- main.py
+++ main.py
@@ -8,33 +8,35 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 @app.route("/")
 def index():
     # choose a movie by invoking our new function
-    movie = get_random_movie()
+    today_movie = get_random_movie()
 
     # build the response string
-    content = "<h1>Movie of the Day</h1>"
-    content += "<ul>"
-    content += "<li>" + movie + "</li>"
-    content += "</ul>"
+    today_content = "<h1>Movie of the Day</h1>"
+    today_content += "<ul>"
+    today_content += "<li>" + today_movie + "</li>"
+    today_content += "</ul>"
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
     tom_movie = get_random_movie()
-    while tom_movie == movie:
+    while tom_movie == today_movie:
         tom_movie = get_random_movie()
 
-    content += "<h1>Tomorrow's Movie</h1>"
-    content += "<ul>"
-    content += "<li>" + tom_movie + "</li>"
-    content += "</ul>"
+    tomorrow_content = "<h1>Tomorrow's Movie</h1>"
+    tomorrow_content += "<ul>"
+    tomorrow_content += "<li>" + tom_movie + "</li>"
+    tomorrow_content += "</ul>"
 
-    return content
+    return today_content + tomorrow_content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
-    movies = ["The Big Lebowski", "Inception", "Harry Potter and the Goblet of Fire", "Land Before Time", "Groundhog Day", "Saving Private Ryan"]
+    movies = ["The Big Lebowski", "Inception", "Harry Potter and the Goblet of Fire", "Land Before Time", "Groundhog Day", "Saving Private Ryan",
+    "Super Troopers"]
     # TODO: randomly choose one of the movies, and return it
-    random_movie = movies[random.randint(0, len(movies) - 1)]
-    return random_movie
+    # random_movie = movies[random.randint(0, len(movies) - 1)]
+    # return random_movie
+    return random.choice(movies)
 
 
 app.run()
